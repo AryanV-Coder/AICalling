66b6b584c5ff36fe844ae2c265b6315fd28dc7a2
AryanV-Coder <aryanv.india1@gmail.com>
2025-05-01 11:28:28 +0530

    ngrok

--- tts_engine.py
+++ tts_engine.py
@@ -9,5 +9,5 @@ def text_to_speech(text):
     filename = f"static/audio/{uuid.uuid4()}.mp3"
     tts = gTTS(text)
     tts.save(filename)
-    public_url = f"https://aicalling-ezst.onrender.com/{filename}"
+    public_url = f"{os.getenv('NGROK_URL')}/{filename}"
     return public_url
